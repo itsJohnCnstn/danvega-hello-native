b849fc87aa1890e172faae4e96075c129e23a86a
Pavel Khankevich <johncnstn@gmail.com>
2026-03-17 19:00:58 +0100

    init

---
+++ src/main/java/dev/johncnstn/danvegahellonative/DanvegaHelloNativeApplication.java
@@ -0,0 +1,13 @@
+package dev.johncnstn.danvegahellonative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanvegaHelloNativeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanvegaHelloNativeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/johncnstn/danvegahellonative/DanvegaHelloNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.johncnstn.danvegahellonative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanvegaHelloNativeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
